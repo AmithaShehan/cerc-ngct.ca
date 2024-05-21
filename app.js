bfe5216c36f959057a1dc08c2173e16ac2979abe
Amitha <112753310+AmithaShehan@users.noreply.github.com>
2024-05-21 17:11:54 +0530

    rev

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - 
+    document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+
+    if(pos>100){
+        scrollProgress.style.display = "grid";
+    }
+    else{
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = 
+    `conic-gradient(#000888 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+}
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
